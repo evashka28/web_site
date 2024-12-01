dcfdff5533041ccbcc919f1fd91de98ba75c8f63
Eva <79522107+evashka28@users.noreply.github.com>
2024-12-01 17:35:09 +0300

    Add files via upload

---
+++ precache-manifest.bcee8a36844d8cdfc62472848b608b3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18be701c7bcc63ccdf8fddf028060a86",
+    "url": "/web_site/index.html"
+  },
+  {
+    "revision": "6810b9e2eb0928054340",
+    "url": "/web_site/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/web_site/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "6810b9e2eb0928054340",
+    "url": "/web_site/static/js/main.b0211b0a.chunk.js"
+  },
+  {
+    "revision": "6f897a89542b5cf4e9cf",
+    "url": "/web_site/static/js/runtime~main.eb3c84b3.js"
+  }
+]);
